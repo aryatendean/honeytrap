674f442d70d14a2d5e101a2709f74eb7452d2d4c
aryatendean <132899120+aryatendean@users.noreply.github.com>
2025-01-16 10:32:46 +0700

    Update commands.go

--- services/redis/commands.go
+++ services/redis/commands.go
@@ -1,61 +1,111 @@
-// Copyright 2016-2019 DutchSec (https://dutchsec.com/)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package redis
-
-type cmd func(*redisService, []interface{}) (string, bool)
-
-var mapCmds = map[string]cmd{
-	"info": (*redisService).infoCmd,
-	// ...
-}
-
-type infoSection func(*redisService) string
-
-var mapInfoCmds = map[string]infoSection{
-	"server":      (*redisService).infoServerMsg,
-	"clients":     (*redisService).infoClientsMsg,
-	"memory":      (*redisService).infoMemoryMsg,
-	"persistence": (*redisService).infoPersistenceMsg,
-	"stats":       (*redisService).infoStatsMsg,
-	"replication": (*redisService).infoReplicationMsg,
-	"cpu":         (*redisService).infoCPUMsg,
-	"cluster":     (*redisService).infoClusterMsg,
-	"keyspace":    (*redisService).infoKeyspaceMsg,
-}
-
-func (s *redisService) infoCmd(args []interface{}) (string, bool) {
-	switch len(args) {
-	case 0:
-		return bulkString(s.infoSectionsMsg(), true), false
-	case 1:
-		_word := args[0].(redisDatum)
-		word, success := _word.ToString()
-		if !success {
-			return "Expected string argument, got something else", false
-		}
-		fn, ok := mapInfoCmds[word]
-		if ok {
-			return bulkString(fn(s), true), false
-		}
-		if word == "default" {
-			return bulkString(s.infoSectionsMsg(), true), false
-		}
-		if word == "all" {
-			return bulkString(s.allSectionsMsg(), true), false
-		}
-		return bulkString("", false), false
-	default:
-		return errorMsg("syntax"), false
-	}
-}
+// Copyright 2016-2019 DutchSec (https://dutchsec.com/)  
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");  
+// you may not use this file except in compliance with the License.  
+// You may obtain a copy of the License at  
+//  
+// http://www.apache.org/licenses/LICENSE-2.0  
+//  
+// Unless required by applicable law or agreed to in writing, software  
+// distributed under the License is distributed on an "AS IS" BASIS,  
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+// See the License for the specific language governing permissions and  
+// limitations under the License.  
+package redis  
+  
+type cmd func(*redisService, []interface{}) (string, bool)  
+  
+var mapCmds = map[string]cmd{  
+	"info":      (*redisService).infoCmd,  
+	"config":    (*redisService).configCmd,  
+	"set":       (*redisService).setCmd,  
+	"saveinfo":  (*redisService).saveInfoCmd,  
+	"flushall":  (*redisService).flushAllCmd,  
+	"command":   (*redisService).commandCmd,  
+}  
+  
+func (s *redisService) configCmd(args []interface{}) (string, bool) {  
+	if len(args) < 1 {  
+		return errorMsg("syntax error"), false  
+	}  
+	subCommand := args[0].(string)  
+	switch subCommand {  
+	case "get":  
+		if len(args) != 2 {  
+			return errorMsg("syntax error"), false  
+		}  
+		key := args[1].(string)  
+		// Implement logic to get the configuration value for the key  
+		value := s.getConfig(key) // Assume this function exists  
+		return bulkString(value, true), false  
+	case "set":  
+		if len(args) != 3 {  
+			return errorMsg("syntax error"), false  
+		}  
+		key := args[1].(string)  
+		value := args[2].(string)  
+		// Implement logic to set the configuration value for the key  
+		s.setConfig(key, value) // Assume this function exists  
+		return "OK", false  
+	default:  
+		return errorMsg("unknown subcommand"), false  
+	}  
+}  
+  
+func (s *redisService) setCmd(args []interface{}) (string, bool) {  
+	if len(args) != 3 {  
+		return errorMsg("syntax error"), false  
+	}  
+	key := args[1].(string)  
+	value := args[2].(string)  
+	// Implement logic to set the key-value pair  
+	s.setKey(key, value) // Assume this function exists  
+	return "OK", false  
+}  
+  
+func (s *redisService) saveInfoCmd(args []interface{}) (string, bool) {  
+	// Implement logic to save information (e.g., to a file or database)  
+	s.saveInfo() // Assume this function exists  
+	return "Information saved", false  
+}  
+  
+func (s *redisService) flushAllCmd(args []interface{}) (string, bool) {  
+	// Implement logic to flush all data  
+	s.flushAll() // Assume this function exists  
+	return "OK", false  
+}  
+  
+func (s *redisService) commandCmd(args []interface{}) (string, bool) {  
+	// Implement logic to return information about the commands  
+	return s.listCommands(), false // Assume this function exists  
+}  
+  
+func (s *redisService) listCommands() string {  
+	// Return a list of available commands  
+	var commands []string  
+	for cmd := range mapCmds {  
+		commands = append(commands, cmd)  
+	}  
+	return bulkString(strings.Join(commands, ", "), true)  
+}  
+  
+func (s *redisService) getConfig(key string) string {  
+	// Placeholder for getting configuration value  
+	return "value" // Replace with actual logic  
+}  
+  
+func (s *redisService) setConfig(key, value string) {  
+	// Placeholder for setting configuration value  
+}  
+  
+func (s *redisService) setKey(key, value string) {  
+	// Placeholder for setting key-value pair  
+}  
+  
+func (s *redisService) saveInfo() {  
+	// Placeholder for saving information  
+}  
+  
+func (s *redisService) flushAll() {  
+	// Placeholder for flushing all data  
+}  
